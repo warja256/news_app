73c292aa77e493ede75d6059ca4f10b4eb6ca001
Varya Kush <barbarakushaeva@gmail.com>
2025-02-23 11:32:00 +0300

    Создание приложения новостей, инициализация библиотек Talker, GetIt, Dio

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:news_app/news_app.dart';
+import 'package:talker_dio_logger/talker_dio_logger.dart';
+import 'package:talker_flutter/talker_flutter.dart';
+
+void main() {
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // Инициализируем Talker для логирования
+    final talker = TalkerFlutter.init();
+    // Регистрация объекта talker в сервис-локатор GetIt
+    GetIt.I.registerSingleton(talker);
+    // Логируем запуск Talker
+    GetIt.I<Talker>().debug('Talker started...');
+
+    // Настройка Dio (HTTP-клиент) с логированием запросов и ответов через Talker
+    final dio = Dio();
+    dio.interceptors.add(
+      TalkerDioLogger(
+        talker: talker, // Передаем Talker для логирования
+        settings: TalkerDioLoggerSettings(
+            printResponseData: false), // Настройки логирования
+      ),
+    );
+
+    // Настройка обработки ошибок Flutter
+    FlutterError.onError =
+        (details) => GetIt.I<Talker>().handle(details.exception, details.stack);
+
+    runApp(const MyApp());
+  },
+      // Обработчик ошибок зоны
+      (e, st) => GetIt.I<Talker>().handle(e, st));
+}
