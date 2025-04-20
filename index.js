21745e6c21bd0c7c6b60360088f21210cd1dc4af
Varya Kush <barbarakushaeva@gmail.com>
2025-04-20 20:39:45 +0300

    добавлен json файл с новостями и подлкючён к апи

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
+app.use(express.json);
 
-const posts = [
-  { id: 1, title: 'First post', body: 'This is the first post.' },
-  { id: 2, title: 'Second post', body: 'This is the second post.' },
-  { id: 3, title: 'Third post', body: 'This is the third post.' }
-];
-
-// Обработка GET-запроса для /posts
 app.get("/posts", (req, res) => {
+  const filePath = path.join(__dirname, 'data/data.json');
+
+  fs.readFile(filePath, "utf8", () => {
+    if (err) {
+      return res.status(500).json({ message: "Error reading data" });
+    }
+
+    const posts = JSON.parse.data();
+    res.status(200).JSON(posts);
+  })
+
+
   res.status(200).json(posts);
 });
 
-// Логирование ошибки, если путь не найден
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not Found' });
-});
-
-const port = process.env.PORT || 9001;
+const port = 9001;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
